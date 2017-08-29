fad6f535fde504724fc83dcbe88ab603c2b98440
Rails Course <railscourse@gmail.com>
2017-08-29 17:53:44 +0000

    CRUD pages for articles, partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all  
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -17,6 +25,28 @@ class ArticlesController < ApplicationController
     end
     
   end
+  
+  def update
+    @article = Article.find(params[:id])
+    if(@article.update(article_params))
+      flash[:notice] = "Article saved successfully";
+      redirect_to article_path(@article)
+    
+    else
+      render 'edit'
+    end
+    
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = "Article successfully deleted."
+    
+    redirect_to articles_path
+  end
+  
   
   def show
     @article = Article.find(params[:id])
